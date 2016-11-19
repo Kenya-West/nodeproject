6bcc389080a1b7f65b03453941aff291bf8bb6f2
Innokenty <kenya-west@outlook.com>
2016-11-19 06:52:46 +0000

    Added bootstrap

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,3 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res) {
+    res.send('Aloha Routing!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log("Server is running on port: " + port);
+});
